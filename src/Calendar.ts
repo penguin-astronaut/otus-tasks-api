a9e81086bc5e8d08909d2188fc1e5d8037438249
Vadim Kasatkin <penguin2804@gmail.com>
2021-07-25 11:16:23 +0400

    calendar constructor storage init

--- src/Calendar.ts
+++ src/Calendar.ts
@@ -1,12 +1,15 @@
 import { AbstractCalendarService } from "./AbstractCalendarService";
+import { JsonDBService } from "./JsonDBService";
 import { LocalStoarageService } from "./LocalStorageService";
 
 export class Calendar {
-  storage: AbstractCalendarService;
+  public storage: AbstractCalendarService;
 
-  constructor(storage?: string) {
-    if (!storage) {
+  constructor(storage: "local" | "json") {
+    if (storage === "local") {
       this.storage = new LocalStoarageService();
+    } else {
+      this.storage = new JsonDBService();
     }
   }
 }
